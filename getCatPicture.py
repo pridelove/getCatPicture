b22cc0d940700dc146e41a3cd3a5e0e7690e6060
Salted fish <38132799+pridelove@users.noreply.github.com>
2020-03-09 01:07:30 +0800

    更新多线程运行

--- getCatPicture.py
+++ getCatPicture.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+import threading
 
 import requests
 
@@ -17,35 +18,46 @@ def getCat(url):
         response = requests.request('get', url, headers=header).text
         cat_picture_list = re.findall(r' data-original="(http://\S+.jpg)', response)
         for i in cat_picture_list:
-            #发现第一次下载的缩略图
             i = i.replace('bmiddle', 'large')
             downCarPicture(i)
     except Exception as e:
         print(e)
         pass
 
+
 # 传入链接下载图片
 def downCarPicture(link):
     picture = requests.get(link)
-    file_name = 'Picture//'+link.split('/')[-1]
+    file_name = 'Picture//' + link.split('/')[-1]
     print('download...', file_name)
     with open(file_name, 'wb+') as f:
         f.write(picture.content)
 
 
 # 输入需要几页的猫图
-def getLink(num):
-    for page in range(1, num + 1):
-        print('开始下载第%d页' % page)
-        url = f"https://www.fabiaoqing.com/search/search/keyword/%E7%8C%AB%E5%92%AA/type/bq/page/{page}.html"
-        getCat(url)
-        time.sleep(3)
-    print("下载完成~")
+def getLink(page):
+    print('开始下载第%d页' % page)
+    url = f"https://www.fabiaoqing.com/search/search/keyword/%E7%8C%AB%E5%92%AA/type/bq/page/{page}.html"
+    getCat(url)
 
-if __name__  == '__main__':
-    #页面猫的表情最多77页
+
+def _request():
+    thread_list = []
+    start = time.time()
+    for i in range(1, 78):
+        temp = threading.Thread(target=getLink, args=(i,))
+        thread_list.append(temp)
+    for t in thread_list:
+        t.start()
+    for t in thread_list:
+        t.join()
+    print("本次用时:", time.time() - start)
+
+
+if __name__ == '__main__':
+    # 页面猫的表情最多77页
     if os.path.exists('Picture'):
-        getLink(77)
+        _request()
     else:
         os.mkdir("Picture")
-        getLink(77)
+        _request()
